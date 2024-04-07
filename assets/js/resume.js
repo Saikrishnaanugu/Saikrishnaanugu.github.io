dd91643c7a0af6a57ead9f09b9c56420f2d1a597
Sai Krishna Anugu <saikrishnaanugu18@gmail.com>
2024-04-07 16:49:09 -0400

    Update resume.js

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -1,23 +1,22 @@
 
-		function downloadPDF() {
-			// Path to the PDF file within your project
-			const pdfPath = '.documents/sai krishna resume .pdf';
-			
-			// Create an anchor element
-			const a = document.createElement('a');
+function downloadPDF() {
+    // Path to the PDF file within your project
+    const pdfPath = '.documents/sai krishna resume .pdf';
+    
+    // Create an anchor element
+    const a = document.createElement('a');
 
-			// Set the href attribute with the path to the PDF file
-			a.href = pdfPath;
-			a.target = '_blank';
+    // Set the href attribute with the path to the PDF file
+    a.href = pdfPath;
+    a.target = '_blank';
 
-			// Set the download attribute with the desired file name
-			a.download = 'sai krishna resume .pdf';
+    // Set the download attribute with the desired file name
+    a.download = 'sai krishna resume .pdf';
 
-			// Append the anchor to the document and trigger a click event
-			document.body.appendChild(a);
-			a.click();
+    // Append the anchor to the document and trigger a click event
+    document.body.appendChild(a);
+    a.click();
 
-			// Remove the anchor from the document
-			document.body.removeChild(a);
-			}
-			
+    // Remove the anchor from the document
+    document.body.removeChild(a);
+    }
