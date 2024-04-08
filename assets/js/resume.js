e3c4f3cdd2628d47dc3a3c46c09c4961d1ff4538
Sai Krishna Anugu <112577289+Saikrishnaanugu@users.noreply.github.com>
2024-04-07 21:22:05 -0400

    Update resume.js

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -1,22 +1,27 @@
+<script>
+document.getElementById('downloadResumeButton').addEventListener('click', function() {
+  window.location.href = 'documents/sai krishna resume .pdf';
+});
+</script>
 
-function downloadPDF() {
-    // Path to the PDF file within your project
-    const pdfPath = '.documents/sai krishna resume .pdf';
+// function downloadPDF() {
+//     // Path to the PDF file within your project
+//     const pdfPath = '.documents/sai krishna resume .pdf';
     
-    // Create an anchor element
-    const a = document.createElement('a');
+//     // Create an anchor element
+//     const a = document.createElement('a');
 
-    // Set the href attribute with the path to the PDF file
-    a.href = pdfPath;					                            
-    a.target = '_blank';
+//     // Set the href attribute with the path to the PDF file
+//     a.href = pdfPath;					                            
+//     a.target = '_blank';
 
-    // Set the download attribute with the desired file name
-    a.download = 'sai krishna resume .pdf';
+//     // Set the download attribute with the desired file name
+//     a.download = 'sai krishna resume .pdf';
 
-    // Append the anchor to the document and trigger a click event
-    document.body.appendChild(a);
-    a.click();
+//     // Append the anchor to the document and trigger a click event
+//     document.body.appendChild(a);
+//     a.click();
 
-    // Remove the anchor from the document
-    document.body.removeChild(a);
-    }
+//     // Remove the anchor from the document
+//     document.body.removeChild(a);
+//     }
